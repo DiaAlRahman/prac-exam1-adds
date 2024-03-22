cad372baf8ffeb8595d9595da80191550d056eb2
DiaAlRahman <127028600+DiaAlRahman@users.noreply.github.com>
2024-03-22 16:26:41 +1030

    commit

--- r1.cpp
+++ r1.cpp
@@ -1,12 +1,12 @@
 #include <math.h>
 
-int sum_of_powers(int n){
-    return sum_of_powers_acc(n, 0);
-}
-
 int sum_of_powers_acc(int n, int acc){
     if (n < 0){
         return acc;
     }
-    return(n-1, acc + pow(n, n));
+    return sum_of_powers_acc(n-1, acc + pow(n, n));
+}
+
+int sum_of_powers(int n){
+    return sum_of_powers_acc(n, 0);
 }
